64e8eab0e0ad05ee916b1bd39b56f0e0975d342c
Mervin Fansler <fanslerm@mskcc.org>
2023-11-29 17:55:28 -0500

    delimiter swap

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 @rem inject include path
 set "POSIX_LIBRARY_INC=%LIBRARY_INC:\=/%"
-sed -i "s/PKG_CXXFLAGS =/PKG_CXXFLAGS = -I\"%POSIX_LIBRARY_INC%\"/" src\Makevars.win
+sed -i "s;PKG_CXXFLAGS =;PKG_CXXFLAGS = -I\"%POSIX_LIBRARY_INC%\";" src\Makevars.win
 
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
