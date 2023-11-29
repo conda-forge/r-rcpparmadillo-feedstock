7768b7f9e961eaa043e02afdba257681bf5e6f01
Mervin Fansler <fanslerm@mskcc.org>
2023-11-29 16:02:46 -0500

    escapes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 @rem inject include path
-sed -i "s/PKG_CXXFLAGS =/PKG_CXXFLAGS = -I%LIBRARY_INC%/" src\Makevars.win
+sed -i "s/PKG_CXXFLAGS =/PKG_CXXFLAGS = -I\"%LIBRARY_INC%\"/" src\Makevars.win
 
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
