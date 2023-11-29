cf15b2b1e1b13ce14716c12224867204047a5fa6
Mervin Fansler <fanslerm@mskcc.org>
2023-11-29 15:09:15 -0500

    inject include path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
-@rem add explicit C++ standard
-echo CXX_STD=CXX14 >> src\Makevars.win
+@rem inject include path
+sed -i "s/PKG_CXXFLAGS =/PKG_CXXFLAGS = -I%LIBRARY_INC%/" src\Makevars.win
 
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
